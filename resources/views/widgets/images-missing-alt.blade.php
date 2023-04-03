f5231dd2830db01c8bcd6811c984a9eaf46915c3
Rob de Kort <rob@studio1902.nl>
2023-04-03 20:54:41 +0200

    Restyle missing alt widget for v4

--- resources/views/widgets/images-missing-alt.blade.php
+++ resources/views/widgets/images-missing-alt.blade.php
@@ -1,15 +1,15 @@
 <div class="card p-0 overflow-hidden h-full">
-    <div class="flex justify-between items-center pt-2 px-2">
+    <div class="flex justify-between items-center p-4">
         <h2>
             <div class="flex items-center">
-                <div class="h-6 w-6 mr-1 text-grey-80">
-                    @cp_svg('assets')
+                <div class="h-6 w-6 mr-2 text-gray-800">
+                    @cp_svg('icons/default/assets')
                 </div>
                 <span>{{ __('strings.widget_assets_title', ['container' => $container]) }}</span>
             </div>
         </h2>
     </div>
-    <div class="content p-2">
+    <div class="content px-4 pb-4">
         <p>
             {{ __('strings.widget_assets_explanation') }}
             {{ trans_choice('strings.widget_assets_count', $amount, ['amount' => $amount]) }}
@@ -24,7 +24,7 @@
         <tr class="sortable-row outline-none" tabindex="0">
             <td>
                 <div class="flex items-center">
-                    <div class="little-dot mr-1 bg-red"></div>
+                    <div class="little-dot mr-2 bg-red-500"></div>
                     <a href="{{ $asset['edit_url'] }}" aria-label="{{ __('strings.widget_assets_edit') }}">{{ $asset['basename'] }}</a>
                 </div>
             </td>
--- src/Widgets/ImagesMissingAlt.php
+++ src/Widgets/ImagesMissingAlt.php
@@ -32,7 +32,7 @@ class ImagesMissingAlt extends Widget
 
         $assets = collect($assets);
 
-        return view('widgets.images-missing-alt', [
+        return view('statamic-peak-tools::widgets.images-missing-alt', [
             'assets' => $assets->slice(0, $this->config('limit', 5)),
             'amount' => count($assets),
             'container' => AssetContainer::findByHandle($this->config('container', 'assets'))->title(),
