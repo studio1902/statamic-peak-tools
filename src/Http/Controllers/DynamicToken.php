6c7b35fe62ee54bf9e5d62f73a5b981efa80a71a
Marco Rieser <marco@rieser.dev>
2023-04-03 14:56:30 +0200

    Allow token refreshing from any host while local (#7)

--- src/Http/Controllers/DynamicToken.php
+++ src/Http/Controllers/DynamicToken.php
@@ -7,21 +7,32 @@ use Illuminate\Http\Request;
 
 class DynamicToken extends Controller
 {
-     /**
+    /**
      * Get refreshed CSRF token.
      *
      * @return string
      */
     public function __invoke(Request $request)
     {
-        $referer = request()->headers->get('referer');
-        $contains = str_contains($referer, request()->getHttpHost());
-        if (empty($referer) || !$contains) {
+        if (!$this->isSafeRequest()) {
             abort(404);
         }
 
         return response()->json([
             'csrf_token' => csrf_token()
         ]);
+    }
+
+    protected function isSafeRequest(): bool
+    {
+        if (!($referer = request()->headers->get('referer'))) {
+            return false;
+        }
+
+        if (in_array(config('app.env'), ['local'])) {
+            return true;
+        }
+
+        return str_contains($referer, request()->getHttpHost());
     }
 }
