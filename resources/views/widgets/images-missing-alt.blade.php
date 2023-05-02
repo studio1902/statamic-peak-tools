8f39438c4a6d838ad6b488adc6be8e821870122a
Rob de Kort <rob@studio1902.nl>
2023-05-02 12:38:35 +0200

    Statamic v4 support (#8)
    * Move ImagesMissingAlt view from SEO addon
    
    * Restyle missing alt widget for v4
    
    * Update CHANGELOG
    
    * Use correct widget icon
    
    * Squashed commit of the following:
    
    commit e0c436c77119018ea9665a828fa8a0f88d5ff99a
    Author: Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
    Date:   Mon Apr 10 10:30:52 2023 +0100
    
        Update URL in pull request template (#9)
    
    * Update Missing Alt Widget styling
    
    * Add support for morphing live preview
    
    * Update CHANGELOG
    
    * Update CHANGELOG

---
+++ resources/views/widgets/images-missing-alt.blade.php
@@ -0,0 +1,42 @@
+<div class="card p-0 overflow-hidden h-full">
+    <div class="flex justify-between items-center p-4">
+        <h2>
+            <div class="flex items-center">
+                <div class="h-6 w-6 mr-2 text-gray-800">
+                    @cp_svg('icons/light/assets')
+                </div>
+                <span>{{ __('strings.widget_assets_title', ['container' => $container]) }}</span>
+            </div>
+        </h2>
+    </div>
+    <div class="content px-4 pb-4">
+        <p>
+            {{ __('strings.widget_assets_explanation') }}
+            {{ trans_choice('strings.widget_assets_count', $amount, ['amount' => $amount]) }}
+        </p>
+    </div>
+
+    @if ($assets)
+        <table tabindex="0" class="data-table">
+            <tbody tabindex="0">
+    @endif
+    @forelse ($assets as $asset)
+        <tr class="sortable-row outline-none" tabindex="0">
+            <td>
+                <div class="flex items-center">
+                    <div class="little-dot mr-2 bg-red-500"></div>
+                    <a href="{{ $asset['edit_url'] }}" aria-label="{{ __('strings.widget_assets_edit') }}">{{ $asset['basename'] }}</a>
+                </div>
+            </td>
+            <td class="actions-column"></td>
+        </tr>
+    @empty
+        <div class="content p-4">
+            <p>{{ __('strings.widget_assets_done') }}</p>
+        </div>
+    @endforelse
+    @if ($assets)
+            </tbody>
+        </table>
+    @endif
+</div>
