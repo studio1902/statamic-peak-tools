d7f89f7aedca84a6e920d8aed6e39647e5cc70b0
Rob de Kort <rob@studio1902.nl>
2023-10-30 13:26:08 +0100

    Remove actions

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,10 +7,6 @@ use Studio1902\PeakTools\Widgets\ImagesMissingAlt;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $routes = [
-        'actions' => __DIR__ . '/../routes/actions.php',
-    ];
-
     protected $widgets = [
         ImagesMissingAlt::class
     ];
