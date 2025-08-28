1f6279d05141804a87f4c32c469342e249c8d9c2
Jason Varga <jason@pixelfear.com>
2025-08-28 13:07:27 -0400

    wip

---
+++ resources/views/cp/widgets/images-missing-alt.blade.php
@@ -0,0 +1,35 @@
+<ui-widget
+    title="{{ __('statamic-peak-tools::default.widget_assets_title', ['container' => $containers[0]]) }}"
+    icon="assets"
+>
+    <div class="px-4 py-3 flex flex-col gap-4 text-sm">
+        <p>
+            {{ __('statamic-peak-tools::default.widget_assets_explanation') }}
+            {{ trans_choice('statamic-peak-tools::default.widget_assets_count', $amount, ['amount' => $amount]) }}
+        </p>
+
+        @if ($assets)
+            <table class="w-full">
+                <tbody>
+                @endif
+                @forelse ($assets as $asset)
+                    <tr>
+                        <td class="p-0.75 text-sm">
+                            <div class="flex items-center gap-2">
+                                <span class="flex items-center gap-2">
+                                    <span class="size-2 rounded-full bg-red-500"></span>
+                                </span>
+
+                                <a href="{{ $asset['edit_url'] }}" aria-label="{{ __('statamic-peak-tools::default.widget_assets_edit') }}">{{ $asset['basename'] }}</a>
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <p>{{ __('statamic-peak-tools::default.widget_assets_done') }}</p>
+                @endforelse
+                @if ($assets)
+                </tbody>
+            </table>
+        @endif
+    </div>
+</ui-widget>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite'
+import statamic from '@statamic/cms/vite-plugin';
 
 export default defineConfig({
     plugins: [
+        statamic(),
         tailwindcss(),
         laravel({
             input: [
