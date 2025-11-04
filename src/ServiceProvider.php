221645793438202c62e5b95a2bc54743d73a3607
Rob de Kort <rob@studio1902.nl>
2025-11-04 08:43:46 +0100

    Conditionally load SEO globals

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -49,8 +49,8 @@ class ServiceProvider extends AddonServiceProvider
             }
 
             Statamic::provideToScript([
-                'use_fathom' => GlobalSet::findByHandle('seo')->inDefaultSite()->get('use_fathom'),
-                'use_google' => GlobalSet::findByHandle('seo')->inDefaultSite()->get('tracker_type') === 'gtm' || GlobalSet::findByHandle('seo')->inDefaultSite()->get('tracker_type') === 'gtag' || GlobalSet::findByHandle('seo')->inDefaultSite()->get('tracker_type') === 'sgtm',
+                'use_fathom' => GlobalSet::findByHandle('seo')?->inDefaultSite()->get('use_fathom'),
+                'use_google' => GlobalSet::findByHandle('seo')?->inDefaultSite()->get('tracker_type') === 'gtm' || GlobalSet::findByHandle('seo')?->inDefaultSite()->get('tracker_type') === 'gtag' || GlobalSet::findByHandle('seo')?->inDefaultSite()->get('tracker_type') === 'sgtm',
             ]);
         });
     }
