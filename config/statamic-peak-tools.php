33c7c69222873c224711cc78a44dfc208979456f
Marco Rieser <marco@rieser.dev>
2025-11-13 18:45:48 +0100

    Extract default picture formats into a config file

---
+++ config/statamic-peak-tools.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'picture' => [
+        'default_formats' => [
+            'avif' => 'image/avif',
+            'webp' => 'image/webp',
+            'jpg' => 'image/jpeg',
+        ]
+    ]
+];
